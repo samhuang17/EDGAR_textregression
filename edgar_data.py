8b4226a9f887d120fb8f5da7f8740207ef5e15bb
Shuo-Chieh Huang <39793567+samhuang17@users.noreply.github.com>
2021-12-20 20:54:35 -0600

    back up

--- edgar_data.py
+++ edgar_data.py
@@ -5,33 +5,33 @@ import os
 # Create vocabulary that has w2v embedding
 
 with open("edgar_w2v_200.txt") as f:
-	lines = f.readlines()[1:]
+    lines = f.readlines()[1:]
 
 w2v_vocab = [line.split()[0] for line in lines]
 
 # Load EDGAR-CORPUS data and calculate term-frequency
 
 def tf_dict(corpus, vocab):
-	corpus_exp = re.findall(r'w\+', corpus.lower())
-	res = {}
-	for word in corpus_exp:
-		if word in vocab:
-			if word in res.keys():
-				res[word] += 1
-			else:
-				res[word] = 1
-	return res
+    corpus_exp = re.findall(r'w\+', corpus.lower())
+    res = {}
+    for word in corpus_exp:
+        if word in vocab:
+            if word in res.keys():
+                res[word] += 1
+            else:
+                res[word] = 1
+    return res
 
 for year in range(2010, 2021):
-	for filename in os.listdir("./{}".format(year)):
-		temp = {}
-		with open("./{}/{}".format(year, filename), 'r') as f:
-			corpus = json.load(f)
-		tf_1A = tf_dict(corpus["section_1A"], w2v_vocab)
-		tf_7 = tf_dict(corpus["section_7"], w2v_vocab)
-		temp = {"year": year, "cik": corpus["cik"], "section_1A": tf_1A, "section_7": tf_7}
-		with open("./tf/{}_{}.json".format(corpus["cik"], str(year)), 'w') as outfile:
-			json.dump(temp, outfile)
+    for filename in os.listdir("./{}".format(year)):
+        temp = {}
+        with open("./{}/{}".format(year, filename), 'r') as f:
+            corpus = json.load(f)
+        tf_1A = tf_dict(corpus["section_1A"], w2v_vocab)
+        tf_7 = tf_dict(corpus["section_7"], w2v_vocab)
+        temp = {"year": year, "cik": corpus["cik"], "section_1A": tf_1A, "section_7": tf_7}
+        with open("./tf/{}_{}.json".format(corpus["cik"], str(year)), 'w') as outfile:
+            json.dump(temp, outfile)
 
 # Create cik_to_tickers_dict dictionary
 
