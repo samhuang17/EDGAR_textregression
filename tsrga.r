0b58cb69d9d30d5194be8dc049229abaab1d9afb
Shuo-Chieh Huang <39793567+samhuang17@users.noreply.github.com>
2021-12-31 15:21:52 -0600

    TSRGA algorithm

--- tsrga.r
+++ tsrga.r
@@ -46,14 +46,14 @@ comp_ip = function(u, X, u_dim, x_dim, L) {
     return(list("IP" = L * abs(u_x), "B" = L * sign(u_x)))
   } else if (u_dim == 1 && x_dim > 1) {
     u_x_norm = sqrt(sum(u_x^2))
-    return(list("IP" = L * u_x_norm, "B" = L * u_x / u_x_norm))
+    return(list("IP" = L * u_x_norm, "B" = L * t(u_x) / u_x_norm))
   } else if (u_dim > 1 && x_dim == 1) {
     u_x_norm = sqrt(sum(u_x^2))
-    return(list("IP" = L * u_x_norm, "B" = L * u_x / u_x_norm))
+    return(list("IP" = L * u_x_norm, "B" = L * t(u_x) / u_x_norm))
   } else {
     dc = svd(u_x, nu = 1, nv = 1)
-    return(list("IP" = L * dc$d[1], "B" = L * outer(as.vector(dc$u), 
-                                                    as.vector(dc$v))))
+    return(list("IP" = L * dc$d[1], "B" = L * outer(as.vector(dc$v), 
+                                                    as.vector(dc$u))))
   }
 }
 
@@ -61,15 +61,17 @@ comp_lambda = function(u, X, B, G) {
   XB = X %*% B
   C = XB - G
   
-  num = tr(t(U) %*% C)
+  num = tr(t(u) %*% C)
   denum = sum(C^2)
   lambda_uc = num / denum
-  
+
   return(max(min(lambda_uc, 1), 0))
 }
 
-rga = function(y, X, dims, L, Kn, mc_cores = 1, parallel = FALSE) {
+rga = function(y, X, dims, L, Kn, G_init = NULL, B_init = NULL,
+               mc_cores = 1, parallel = FALSE) {
   ###
+  # rga: relaxed greedy algorithm
   # inputs:
   # y: an n by M matrix of the response variables, where M = number of tasks.
   # X: a list of covariate observation matrices. X should have p elements, each 
@@ -78,6 +80,7 @@ rga = function(y, X, dims, L, Kn, mc_cores = 1, parallel = FALSE) {
   #       of each of the predictors
   # L: user-prescribed parameter
   # Kn: number of desired iterations
+  # G_init: initial guess for the fitted values; default is zero.
   # mc_cores: number of cores available for parallel computing
   # parallel: whether to perform parallel computing for the p variables.
   #
@@ -95,10 +98,28 @@ rga = function(y, X, dims, L, Kn, mc_cores = 1, parallel = FALSE) {
     n = dim(y)[1]
   }
   
-  B = vector(mode = "list", length = p)
-  u = y
-  G = 0
-  loss = sum(y^2) / n
+  if (is.null(B_init)) {
+    B = vector(mode = "list", length = p)
+  } else {
+    B = B_init
+  }
+  if (is.null(G_init) && is.null(B_init)) {
+    G = 0
+  } else if (is.null(G_init) && !is.null(B_init)) {
+    G = 0
+    for (j in 1:p) {
+      if (!is.null(B[[j]])) {
+        G = G + X[[j]] %*% B[[j]]
+      }
+    }
+  } else {
+    # remember to check dimension
+    G = G_init
+  }
+  
+  u = y - G
+  loss = rep(NA, Kn)
+  lambda_seq = rep(NA, Kn)
   J_hat = NULL
   
   rga_search = function(j) {
@@ -115,9 +136,10 @@ rga = function(y, X, dims, L, Kn, mc_cores = 1, parallel = FALSE) {
     j_star = which.max(ips)
     J_hat = c(J_hat, j_star)
     B_tilde = comp_ip(u, X[[j_star]], u_dim = dims[1], x_dim = dims[j_star + 1],
-                      L = L)
+                      L = L)$B
     lambda = comp_lambda(u, X[[j_star]], B_tilde, G)
-    
+    lambda_seq[i] = lambda
+
     for (j in 1:p) {
       if (j == j_star) {
         if (is.null(B[[j]])) {
@@ -132,21 +154,37 @@ rga = function(y, X, dims, L, Kn, mc_cores = 1, parallel = FALSE) {
       }
     }
     
-    G = (1 - lambda) * G + lambda * (X[[j]] %*% B_tilde)
+    G = (1 - lambda) * G + lambda * (X[[j_star]] %*% B_tilde)
+    u = y - G
+    loss[i] = sum(u^2) / n
   }
   
-  return(list("B" = B, "J_hat" = unique(J_hat), "path" = J_hat, "G" = G))
+  return(list("B" = B, "J_hat" = unique(J_hat), "path" = J_hat, "G" = G,
+              "loss" = loss, "lambda_seq" = lambda_seq))
 }
 
-
-
-
-
-
-
-
-
-
-
-
+tsrga = function(y, X, dims, L, Kn1, Kn2, 
+                 mc_cores = 1, parallel = FALSE) {
+  ###
+  # tsrga: two-stage relaxed greedy algorithm
+  ###
+  res1 = rga(y, X, dims, L, Kn1, mc_cores = mc_cores, parallel = parallel)
+  selected = sort(res1$J_hat)
+  selected_X = X[selected]
+  B_init = res1$B[!sapply(res1$B, is.null)]
+  new_dims = c(dims[1], dims[1 + selected])
+  res2 = rga(y, selected_X, new_dims, L, Kn2, G_init = res1$G, B_init = B_init,
+             mc_cores, parallel)
+  
+  p = length(dims) - 1
+  B_hat = vector(mode = "list", length = p)
+  for (j in 1:p) {
+    if (j %in% selected) {
+      B_hat[j] = res2$B[which(selected == j)]
+    }
+  }
+  
+  return(list("B" = B_hat, "J_hat" = selected, 
+              "path" = c(res1$path, selected[res2$path]), "G" = res2$G))
+}
 
