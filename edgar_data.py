c888c3930b824c298975bd447369d4564a369384
Shuo-Chieh Huang <39793567+samhuang17@users.noreply.github.com>
2022-01-08 13:57:40 -0600

    load edgar data

--- edgar_data.py
+++ edgar_data.py
@@ -1,12 +1,12 @@
 import json
 import re
 import os
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 
-### Create cik_to_tickers_dict dictionary
+### Create cik_to_tickers dictionary
 
-file_name = "company_tickers.json" # downloaded from https://www.sec.gov/files/company_tickers.json
+file_name = "company_tickers.json"
 cik_to_tickers_dict = {}
 
 f = open(file_name)
@@ -16,72 +16,82 @@ for company in file.values():
         cik_to_tickers_dict[str(company["cik_str"])].append(company["ticker"])
     else:
         cik_to_tickers_dict[str(company["cik_str"])] = [company["ticker"]]
+    
 f.close()
 
-### Create vocabulary that has w2v embedding
+### Construct vocabulary that has w2v embeddings
 
 with open("edgar_w2v_200.txt") as f:
     lines = f.readlines()[1:]
 
 w2v_vocab = [line.split()[0] for line in lines]
 
+### Load EDGAR-CORPUS data
+
+def tf_dict(corpus, vocab):
+    corpus_exp = re.findall(r'\w+', corpus.lower())
+    res = {}
+    for word in corpus_exp:
+        if word in vocab:
+            if word in res.keys():
+                res[word] += 1
+            else:
+                res[word] = 1
+    return res
+
+for year in range(2010, 2020):
+    for filename in os.listdir("./{}".format(year)):
+        temp = {}
+        with open("./{}/{}".format(year, filename), 'r') as f:
+            corpus = json.load(f)
+        tf_7A = tf_dict(corpus["section_7A"], w2v_vocab) # only include terms that are in w2v_vocab
+        temp = {"year": year, "cik": corpus["cik"], "section_7A": tf_7A}
+        with open("./tf/{}_{}.json".format(corpus["cik"], str(year)), 'w') as outfile:
+            json.dump(temp, outfile)
+
 ### Construct tf-idf
-term_doc_freq_1A = {}
-term_doc_freq_7 = {}
+
+term_doc_freq_7A = {}
 N = 0
 
 for filename in os.listdir("./tf"):
-    N += 1
     with open("./tf/{}".format(filename)) as f:
         tf = json.load(f)
-    for word in tf["section_1A"]:
-        if str(word) in term_doc_freq_1A:
-            term_doc_freq_1A[str(word)] += 1
-        else:
-            term_doc_freq_1A[str(word)] = 1
-    for word in tf["section_7"]:
-        if str(word) in term_doc_freq_7:
-            term_doc_freq_7[str(word)] += 1
-        else:
-            term_doc_freq_7[str(word)] = 1
+        # tf only contains tokens that are in w2v_vocab
+    if len(tf["section_7A"]) > 15: # only conisder documents with more than 15 distinct tokens
+        N += 1
+        for word in tf["section_7A"]:
+            if str(word) in term_doc_freq_7A:
+                term_doc_freq_7A[str(word)] += 1
+            else:
+                term_doc_freq_7A[str(word)] = 1
 
-top_1A = {}
-top_7 = {}
+top_7A = {}
 
 for filename in os.listdir("./tf"):
     with open("./tf/{}".format(filename)) as f:
         tf = json.load(f)
-    temp1 = {}
-    temp2 = {}
-    tot_words_1A = sum(tf["section_1A"].values())
-    tot_words_7 = sum(tf["section_7"].values())
-    for word in tf["section_1A"]:
-        if term_doc_freq_1A[str(word)] < 30:
-            temp1[str(word)] = 0
-        else:
-            temp1[str(word)] = ((tf["section_1A"][str(word)]) / tot_words_1A) * np.log(N / term_doc_freq_1A[str(word)])
-    if temp1:
-        top_1A[(tf["cik"], tf["year"])] = sorted(temp1, key = temp1.get, reverse = True)[:20]
+    if len(tf["section_7A"]) > 15: # only count documents with more than 15 distinct tokens
+        temp = {}
+        tot_words_7A = sum(tf["section_7A"].values())
+        for word in tf["section_7A"]:
+            if str(word) not in temp: 
+                temp[str(word)] = ((tf["section_7A"][str(word)]) / tot_words_7A) * np.log(N / term_doc_freq_7A[str(word)])
+            else:
+                print("duplicate word found: {} in {}, Section 7A".format(word, filename))
+        if temp:
+            top_words = sorted(temp, key = temp.get, reverse = True)[:30]
+            top_7A[(tf["cik"], tf["year"])] = [(word, temp[word]) for word in top_words]
 
-    for word in tf["section_7"]:
-        if term_doc_freq_7[str(word)] < 30:
-            temp2[str(word)] = 0
-        else:
-            temp2[str(word)] = ((tf["section_7"][str(word)]) / tot_words_7) * np.log(N / term_doc_freq_7[str(word)])
-    if temp2:
-        top_7[(tf["cik"], tf["year"])] = sorted(temp2, key = temp2.get, reverse = True)[:20]
-
-
-### Extract word embeddings for top words
+### Extract tfidf-weighted word embeddings for top words
 
 cik_span = {}
-for cik, year in top_7:
-    if len(top_7[(cik, year)]) >= 15:
-        if cik in cik_span:
-            cik_span[cik].append(year)
-        else:
-            cik_span[cik] = [year]
-
+for cik, year in top_7A:
+    if cik in cik_span:
+        cik_span[str(cik)].append(year)
+    else:
+        cik_span[str(cik)] = [year]
+        
 cik_span_10to19 = [cik for cik, year in cik_span.items() if set(range(2010, 2020)).issubset(set(year))]
 
 with open("edgar_w2v_200.txt") as f:
@@ -91,42 +101,15 @@ with open("edgar_w2v_200.txt") as f:
 for i in range(15):
     final_data = []
     for cik in cik_span_10to19:
-        if cik in cik_to_tickers_dict.keys():
+        if cik in cik_to_tickers_dict:
             for year in range(2010, 2020):
-                word = top_7[(cik, year)][i]
-                datum = [cik, cik_to_tickers_dict[cik][0], year] + w2v_embeddings[word]
+                word = top_7A[(cik, year)][i][0]
+                tfidf = top_7A[(cik, year)][i][1]
+                denom = np.sum([top_7A[(cik, year)][j][1] for j in range(15)])
+                datum = [cik, cik_to_tickers_dict[cik][0], year] + [v * tfidf / denom for v in w2v_embeddings[word]]
                 final_data.append(datum)
     df = pd.DataFrame(final_data)
     df.to_csv("./top_w2v/top_{}.csv".format(i), index = False, header = False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
